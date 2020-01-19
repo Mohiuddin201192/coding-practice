76a6299126b7506e3a9c806eb109a79bb4aee149
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-01-19 22:48:42 +0200

    exercise 10 done

--- part1/printing/exercise_10/Program.cs
+++ part1/printing/exercise_10/Program.cs
@@ -6,7 +6,9 @@ namespace exercise_10
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            string name = "Ada ";
+            string profession =" Data Scientist";
+            Console.WriteLine("Once upon a time there was a" + profession + " called "  +  name + "\n" + "On her way to work " +  name + "often pondered what being" +  profession + " meant to them." + "\n" + "When you work as a" +  profession + " you meet interesting people." + "\n" + name  +  "enjoys their work as" +  profession+", The end.");
 
         }
     }
