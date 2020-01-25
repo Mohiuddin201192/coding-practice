4723dc02c7c0e46ac40b8a6eb2993ee253a5fea8
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-01-25 23:39:47 +0200

    ex 28 done

--- part1/conditionals/exercise_28/Program.cs
+++ part1/conditionals/exercise_28/Program.cs
@@ -6,7 +6,17 @@ namespace exercise_28
   {
     public static void Main(string[] args)
     {
-      
+      Console.WriteLine("How old are you?");
+      string number = Console.ReadLine();
+      int age = Convert.ToInt32(number);
+      if (age < 18)
+      {
+        Console.WriteLine("You're under age!");
+      }
+      else
+      {
+        Console.WriteLine("You're an adult!");
+      }
 
     }
   }
