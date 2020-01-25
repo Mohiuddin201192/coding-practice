e0a651256c34b634fc7847b2aa3bf3c86650b0f5
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-01-25 23:26:17 +0200

    ex 27 done

--- part1/conditionals/exercise_27/Program.cs
+++ part1/conditionals/exercise_27/Program.cs
@@ -6,7 +6,20 @@ namespace exercise_27
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Give a number:");
+      string number = Console.ReadLine();
+      int value = Convert.ToInt32(number);
+      if (value >= 0)
+
+      {
+        Console.WriteLine("It is positive");
+        
+      }
+      if (value < 0)
+      {
+        Console.WriteLine("It is not positive");
+      }
+    
 
     }
   }
