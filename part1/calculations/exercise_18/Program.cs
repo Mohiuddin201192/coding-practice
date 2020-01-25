a7afeaf9b91a477c2e58af93844f1246a53b35a4
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-01-25 14:49:58 +0200

    ex 18 done

--- part1/calculations/exercise_18/Program.cs
+++ part1/calculations/exercise_18/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_18
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.Write("Give the first number!\n");
+      String userinput1 = Console.ReadLine();
+      int intvalue1 = Convert.ToInt32(userinput1);
+      Console.Write("Give the second number!\n");
+      string userinput2 = Console.ReadLine();
+      int intvalue2 = Convert.ToInt32(userinput2);
+      Console.Write("Give the third number!\n");
+      string userinput3 = Console.ReadLine();
+      int intvalue3 = Convert.ToInt32(userinput3);
+      int sum = intvalue1 + intvalue2 + intvalue3;
+      Console.WriteLine("The sum is " + sum);
 
     }
   }
