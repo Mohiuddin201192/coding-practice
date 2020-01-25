30906872540327da6a19ad62749aebac89cc604b
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-01-25 16:27:07 +0200

    ex 22 done

--- part1/calculations/exercise_22/Program.cs
+++ part1/calculations/exercise_22/Program.cs
@@ -7,7 +7,21 @@ namespace exercise_22
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string num1 = Console.ReadLine();
+      int value1 = Convert.ToInt32(num1);
+
+      Console.WriteLine("Give the second number!");
+      string num2 = Console.ReadLine();
+      int value2 = Convert.ToInt32(num2);
+
+      Console.WriteLine("Give the third number!");
+      string num3 = Console.ReadLine();
+      int value3 = Convert.ToInt32(num3);
+
+      double average = (value1 + value2 + value3) / 3.0;
+
+      Console.WriteLine("The average is " + average);
 
     }
   }
