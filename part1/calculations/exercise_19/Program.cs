8e2a19429637b991c3fd193cc6f82c32f0724d33
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-01-25 15:31:55 +0200

    ex 19 done

--- part1/calculations/exercise_19/Program.cs
+++ part1/calculations/exercise_19/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_19
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.Write("Give the first number!\n");
+      string me1 = Console.ReadLine();
+      int value1 = Convert.ToInt32(me1);
+      Console.Write("Give the second number!\n");
+      string me2 = Console.ReadLine();
+      int value2 = Convert.ToInt32(me2);
+      int sum = value1 + value2;
+      Console.WriteLine(me1 + " + " + me2 + " = " + (sum));
+      
+
+
 
     }
   }
