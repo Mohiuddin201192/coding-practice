412bae5b905f06fd16db6cf3313f9e2796cb4e0c
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-01-26 18:01:19 +0200

    ex-36 done

--- part1/repetition/exercise_36/Program.cs
+++ part1/repetition/exercise_36/Program.cs
@@ -6,7 +6,24 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        string num = Console.ReadLine();
+        int value = Convert.ToInt32(num);
+        if (value > 0)
+        {
+          Console.WriteLine(value * value);
+        }
+        if (value < 0)
+        {
+          Console.WriteLine("That is negative");
+        }
+        if (value == 0)
+        {
+          break;
+        }
+      }
 
     }
   }
