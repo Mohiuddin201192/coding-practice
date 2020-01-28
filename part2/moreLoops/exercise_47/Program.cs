d857c9d741408b1b57c1be79a1a52aeb371591f0
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-01-29 00:45:29 +0200

    ex 47 done

--- part2/moreLoops/exercise_47/Program.cs
+++ part2/moreLoops/exercise_47/Program.cs
@@ -7,18 +7,23 @@ namespace exercise_47
     public static void Main(string[] args)
     {
 
+      
       Console.WriteLine("Where to?");
-      int num = Convert.ToInt32(Console.ReadLine());
-      for (num = 0; num < 10; num++)
-      {
-        Console.WriteLine(num);
-      }
-      Console.WriteLine("Where from?");
       int num1 = Convert.ToInt32(Console.ReadLine());
-      for (num1 = 0; num1 > 10; num1++)
+
+      Console.WriteLine("Where from?");
+      int num2 = Convert.ToInt32(Console.ReadLine());
+
+      int value = num2;
+
+      while (value <= num2)
       {
-        Console.WriteLine(num1);
+        Console.WriteLine(value);
+        value--;
+        
       }
+      
+      
       
       
       
