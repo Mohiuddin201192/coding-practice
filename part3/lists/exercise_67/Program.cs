8dc5edf0e83ec4e84441869888e6f783261ec553
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-02-16 15:29:31 +0200

    ex_67 done

--- part3/lists/exercise_67/Program.cs
+++ part3/lists/exercise_67/Program.cs
@@ -17,6 +17,9 @@ namespace exercise_67
         }
         list.Add(input);
       }
+      int last = list.Count;
+      Console.WriteLine(list[0]);
+      Console.WriteLine(list[list.Count-1]);
 
     }
   }
