061867567177282409718d57b4cfed55d7f58701
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-02-23 17:32:04 +0200

    ex_89 done

--- part3/strings/exercise_89/Program.cs
+++ part3/strings/exercise_89/Program.cs
@@ -7,7 +7,15 @@ namespace exercise_89
   {
     public static void Main(string[] args)
     {
-
+      while(true)
+      {
+      string text = Console.ReadLine();
+      string[] pieces = text.Split(" ");
+      if (text == "")
+      {
+        break;
+      }
+      Console.WriteLine(pieces[pieces.Length -1]);
 
     }
   }
