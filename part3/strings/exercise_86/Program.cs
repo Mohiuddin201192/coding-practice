b4eeb87a210ccee7a898540ad49f489ed3e15b76
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-02-23 15:15:07 +0200

    ex_86 done

--- part3/strings/exercise_86/Program.cs
+++ part3/strings/exercise_86/Program.cs
@@ -8,27 +8,24 @@ namespace exercise_86
     public static void Main(string[] args)
     {
       while (true)
+    { 
+      string text = Console.ReadLine();
+      if (text == "")
       {
-        string question = Console.ReadLine();
-        if (question == "")
-        // break if empty
-        {
-          break;
-        }
-        //split the question into parts[0] parts[1]
-        // create an array called 'parts'
-        // method split takes a parameter
-        // split the string into name   age
-        string[] parts = question.Split (" ");
-        foreach (string line in parts)
-        {
-          Console.WriteLine(line);
-        }
+        break;
       }
-        
+      string[] pieces = text.Split(" ");
+      for (int i = 0; i < pieces.Length; i++)
+      {
+        Console.WriteLine(pieces[i]);
+      }
 
+  
+    
+    }
 
     }
   }
+  
 }
 
