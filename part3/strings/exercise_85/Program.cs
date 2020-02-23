4a2010de6a41a81aa9583328360425e7d2061ccf
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-02-23 14:36:57 +0200

    ex_85 done

--- part3/strings/exercise_85/Program.cs
+++ part3/strings/exercise_85/Program.cs
@@ -7,6 +7,25 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Enter username:");
+      string name = Console.ReadLine();
+      
+      Console.WriteLine("Enter password:");
+      string password = Console.ReadLine();
+      if (name == "alex" && password =="sunshine")
+      {
+        Console.WriteLine("You have successfully logged in!");
+      }
+      else if (name == "emma" && password == "haskell")
+      {
+        Console.WriteLine("You have successfully logged in!");
+      }
+      else
+      {
+        Console.WriteLine("Incorrect username or password!");
+      }
+      
+
 
 
     }
