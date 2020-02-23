fd38e5ba05e5ba4a1ba0e897ee2a3caa9cd69f0a
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-02-23 17:20:05 +0200

    ex_88 done

--- part3/strings/exercise_88/Program.cs
+++ part3/strings/exercise_88/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_88
   {
     public static void Main(string[] args)
     {
-
+      while(true)
+      {
+      string text = Console.ReadLine();
+      string[] pieces = text.Split(" ");
+      if (text == "")
+      {
+        break;
+      }
+      Console.WriteLine(pieces[0]);
+    }
     }
   }
 }
