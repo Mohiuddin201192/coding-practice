fc595ee734098909d8fe9c8216eeccdafa69ede1
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-02-24 16:33:09 +0200

    ex_92 done

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -6,7 +6,43 @@ namespace exercise_92
   class Program
   {
     public static void Main(string[] args)
-    {
+    { 
+      int oldest = 0;
+      int age = 0;
+      string name = "";
+      int lname = 0;
+      int longest = 0;
+      string longname = "";
+      
+      
+      while (true)
+      {
+        string input = Console.ReadLine();
+        if (input =="")
+        {
+          break;
+        }
+        string[] pieces = input.Split(",");
+        age = 2020 - Convert.ToInt32(pieces[1]);
+        name = pieces[0];
+        lname = name.Length;
+        if (oldest < age)
+
+        {
+          oldest = age;
+        }
+        
+        if (longest < lname)
+        {
+          longest = lname;
+          longname = name;
+        }
+      }
+      age = 2020 - oldest;
+      Console.WriteLine("Longest name: " + name);
+      Console.WriteLine("Highest age: " + age);
+    
+
 
     }
   }
