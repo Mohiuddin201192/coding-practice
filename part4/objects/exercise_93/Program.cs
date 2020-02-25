69db5e7806193ff264d5d9c258b533729f9cd44e
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-02-25 02:50:23 +0200

    ex_93 done

--- part4/objects/exercise_93/Program.cs
+++ part4/objects/exercise_93/Program.cs
@@ -8,6 +8,9 @@ namespace exercise_93
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+      Account myAccount = new Account("My account",100.00);
+      myAccount.Deposit(20.0);
+      Console.WriteLine(myAccount.balance);
 
     }
   }
