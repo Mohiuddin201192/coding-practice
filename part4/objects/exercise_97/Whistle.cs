87abfa7b31b69e487f41360a03ff5c115b610643
Mohiuddin-Al Belal <mohiuddin-albelal@Mohiuddin-Als-MacBook-Pro.local>
2020-02-25 04:23:13 +0200

    ex_97 done

---
+++ part4/objects/exercise_97/Whistle.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace exercise_97
+{
+    public class Whistle
+    {
+        private string sound;
+        public Whistle(string whistleSound)
+        {
+            this.sound = whistleSound;
+        }
+    
+    public  void sound()
+    {
+        Console.WriteLine(this.sound);
+    }
+}
+}
