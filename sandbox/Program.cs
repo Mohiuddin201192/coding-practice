ad7909a8fa5ab5d95c2984452f5dc33d6d895eb3
Mohiuddin201192 <mohiuddin-al.belal@centria.fi>
2020-02-29 22:42:01 +0200

    ex_110 done

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -2,15 +2,45 @@
 
 namespace sandbox
 {
-    class Program
+    public class Person
+{
+  public string name { get; }
+  public int age { get; set; }
+  public int weight { get; set; }
+  public int height { get; set; }
+
+  public Person(string name)
+  {
+    this.age = 0;
+    this.weight = 0;
+    this.height = 0;
+    this.name = name;
+  }
+
+  public double BodyMassIndex()
+  {
+    double heigthPerHundred = this.height / 100.0;
+    return this.weight / (heigthPerHundred * heigthPerHundred);
+  }
+
+  public void GrowOlder()
+  {
+    if (this.age < 100)
     {
-        static void Main(string[] args)
-        {
-        int i = 0;
-    while (i < numOfTimes) 
-    {
-      Console.WriteLine("Greetings!");
-      i++;
+      this.age = this.age + 1;
     }
-    }
+  }
+
+  public bool IsOfLegalAge()
+  {
+    return this.age >= 18;
+  }
+
+
+  public override string ToString()
+  {
+    return this.name + ", age " + this.age + " years";
+  }
+}
+   
 }
