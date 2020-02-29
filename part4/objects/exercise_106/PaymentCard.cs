05e9d1776a1ddf7fc574bf7d4a7d117512383c5b
Mohiuddin201192 <mohiuddin-al.belal@centria.fi>
2020-02-29 19:17:22 +0200

    ex_106 done

---
+++ part4/objects/exercise_106/PaymentCard.cs
@@ -0,0 +1,19 @@
+using System;
+namespace exercise_106
+{
+  public class PaymentCard
+  {
+    private double balance;
+
+    public PaymentCard(double openingBalance)
+    {
+      // write code here
+      this.balance = openingBalance;
+    }
+
+    public override string ToString()
+    {
+      return "The card has a balance of " + this.balance + " euros";
+    }
+  }
+}
