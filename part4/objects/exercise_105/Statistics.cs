5f2e548a7cba672a21eef31df377f5a87463ae76
Mohiuddin201192 <mohiuddin-al.belal@centria.fi>
2020-02-29 18:48:10 +0200

    ex_105 done

--- part4/objects/exercise_105/Statistics.cs
+++ part4/objects/exercise_105/Statistics.cs
@@ -7,12 +7,14 @@ namespace exercise_105
 
     public Statistics()
     {
-      // initialize the variables here
+      this.count = 0;
+      this.sum = 0;
     }
 
     public void AddNumber(int number)
     {
-      // write code here
+      this.count = this.count + 1;
+      sum = sum + number;
       
     }
   }
