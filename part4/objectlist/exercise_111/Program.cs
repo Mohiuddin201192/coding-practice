8a3dc93338a746e71a7704311f024f7d21d11d2e
Mohiuddin201192 <mohiuddin-al.belal@centria.fi>
2020-03-01 15:10:21 +0200

    ex-111 done

--- part4/objectlist/exercise_111/Program.cs
+++ part4/objectlist/exercise_111/Program.cs
@@ -7,9 +7,31 @@ namespace exercise_111
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
       List<PersonalInformation> list = new List<PersonalInformation>();
+      
+      while (true)
+      {
+      Console.WriteLine("First name:");
+      string fname = Console.ReadLine();
+      if (fname == "")
+      {
+        break;
+      }
 
+      Console.WriteLine("Last name:");
+      string lname = Console.ReadLine();
+
+      Console.WriteLine("Identification number:");
+      string inum = Console.ReadLine();
+
+      list.Add(new PersonalInformation(fname, lname, inum));
+      }
+      Console.WriteLine();
+
+      foreach( PersonalInformation item in list)
+      {
+        Console.WriteLine(item.firstName + " " + item.lastName);
+      }
     }
   }
 }
