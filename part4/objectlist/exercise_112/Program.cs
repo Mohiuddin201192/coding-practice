cd051c979929baa02e975473eceba1220c96b2bb
Mohiuddin201192 <mohiuddin-al.belal@centria.fi>
2020-03-01 16:34:48 +0200

    ex-112 done

--- part4/objectlist/exercise_112/Program.cs
+++ part4/objectlist/exercise_112/Program.cs
@@ -7,8 +7,36 @@ namespace exercise_112
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+      List<TelevisionProgram> list = new List<TelevisionProgram>();
 
+      while (true)
+      {
+        Console.Write("Name: ");
+        string Name = Console.ReadLine();
+
+        if (Name == "")
+        {
+          break;
+        }
+        Console.Write("Duration: ");
+        int duration = Convert.ToInt32(Console.ReadLine());
+
+        list.Add (new TelevisionProgram(Name, duration));
+      }
+        Console.WriteLine();
+        Console.Write("Program's maximum duration? ");
+        int minutes =Convert.ToInt32(Console.ReadLine());
+
+        foreach (TelevisionProgram item in list)
+        {
+          if (item.duration <= minutes)
+        
+        {
+          Console.WriteLine (item);
+        }
+      }
+      
+        
 
     }
   }
