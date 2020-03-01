1ac45a77de476096bc78939f231498c7c6cf1f15
Mohiuddin201192 <mohiuddin-al.belal@centria.fi>
2020-03-01 21:19:02 +0200

    ex_114 done

--- part4/files/exercise_114/Program.cs
+++ part4/files/exercise_114/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_114
     {
         public static void Main(string[] args)
         {
+            int num = 0;
 
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                num = num + 1;
+            }
+            Console.WriteLine(num);
         }
     }
 }
