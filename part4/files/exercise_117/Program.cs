b25a093a977805c0de371938c6dbe3784038b804
Mohiuddin201192 <mohiuddin-al.belal@centria.fi>
2020-03-02 01:39:33 +0200

    ex_117 done

--- part4/files/exercise_117/Program.cs
+++ part4/files/exercise_117/Program.cs
@@ -7,8 +7,25 @@ namespace exercise_117
   {
     public static void Main(string[] args)
     {
-      // Ask the user for the file name and print the content of the file
+      Console.WriteLine("Which file should have its contents printed?");
 
+      string input = Console.ReadLine();
+      string [] lines = File.ReadAllLines(input);
+      foreach (string line in lines)
+      {
+        Console.WriteLine(line);
+      }
+      
+     /*  if (input == "data.txt")
+      {
+        string data = File.ReadAllText("data.txt");
+        Console.WriteLine(data);
+      } 
+      if (input == "song.txt")
+      {
+        string song = File.ReadAllText("song.txt");
+        Console.WriteLine(song);
+      } */
       
     }
   }
