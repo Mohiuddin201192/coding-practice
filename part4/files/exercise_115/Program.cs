ab40bb79996131e8d82ca60979b3b6969e331ae3
Mohiuddin201192 <mohiuddin-al.belal@centria.fi>
2020-03-01 21:40:23 +0200

    ex_115 done

--- part4/files/exercise_115/Program.cs
+++ part4/files/exercise_115/Program.cs
@@ -6,7 +6,16 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
-
+      while(true)
+    {
+      string input = Console.ReadLine();
+      if (input == "end")
+      {
+        break;
+      }
+      int num = Convert.ToInt32(input);
+      Console.WriteLine(num * num * num);
     }
+  }
   }
 }
